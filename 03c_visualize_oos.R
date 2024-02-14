2194209c2ad4c7c4e13a42db08dad8bc1e3c8135
Nathaniel Henry <nat.henry7@gmail.com>
2024-02-13 23:18:26 -0800

    Add a third model for comparison, the 'National average' model.

--- 03c_visualize_oos.R
+++ 03c_visualize_oos.R
@@ -33,14 +33,21 @@ dir.create(viz_dir, showWarnings = FALSE)
 
 # Load input data
 oos_full <- config$read('model_results', 'oos_summary')
-oos_full[, plot_label := "Full model"]
+oos_full[, plot_label := "Joint model"]
 oos_prev_only <- config_p$read('model_results', 'oos_summary')
-oos_prev_only[, plot_label := "Model based on survey data only"]
+oos_prev_only[, plot_label := "Prevalence-only model"]
 measure_cols <- c('mean', 'median', 'lower', 'upper')
 for(measure_col in measure_cols){
   oos_full[[measure_col]] <- oos_full[[measure_col]] * 1e5
   oos_prev_only[[measure_col]] <- oos_prev_only[[measure_col]] * 1e5
 }
+
+oos_avg <- (
+  copy(oos_full)[, .(uid, sampsize, ptb_bc, prev_per_100k_obs)]
+  [, c('lower', 'upper', 'median') := NA_real_ ]
+  [, plot_label := "National average model"]
+)
+oos_avg$mean <- sapply(oos_avg[, .I], function(row_i) oos_avg[-row_i, sum(ptb_bc)/sum(sampsize)*1e5])
 
 # Estimate duration when completeness is 100%
 min_duration <- config$get('duration', 'intercept') - config$get('duration', 'slope')
@@ -55,7 +62,8 @@ inc_floor <- (inc_data
 ## Prepare data for side-by-side scatters
 scatter_keep_cols <- c('uid', 'sampsize', 'prev_per_100k_obs', 'plot_label', measure_cols)
 scatter_dt <- rbindlist(list(
-  oos_full[, ..scatter_keep_cols], oos_prev_only[, ..scatter_keep_cols]
+  oos_full[, ..scatter_keep_cols], oos_prev_only[, ..scatter_keep_cols],
+  oos_avg[, ..scatter_keep_cols]
 ))
 
 # Merge on "prevalence floor" data
@@ -72,7 +80,7 @@ scatter_fig <- ggplot(
   data = scatter_dt,
   aes(x = prev_per_100k_obs, y = mean, ymin = lower, ymax = upper)
 ) +
-  facet_wrap('plot_label') + 
+  facet_wrap('plot_label', ncol = 2) + 
   geom_abline(intercept=0, slope=1, linetype=2, lwd=.5, color='#888888') +
   geom_crossbar(alpha=.5) +
   geom_point(size=2, aes(fill = sampsize), shape=21) +
@@ -84,8 +92,9 @@ scatter_fig <- ggplot(
     fill='Prevalence survey\nsample size',
     x = 'Unadjusted survey prevalence', y = 'Out-of-sample modeled TB prevalence'
   ) +
-  theme_bw()
-png(file.path(viz_dir, 'oos_data_scatter.png'), height=6, width=10, units='in', res=300)
+  theme_bw() +
+  theme(legend.position = c(.75, .25))
+png(file.path(viz_dir, 'oos_data_scatter.png'), height=7.5, width=7.5, units='in', res=300)
 print(scatter_fig)
 dev.off()
 
@@ -93,9 +102,9 @@ dev.off()
 ## Plot results against minimum possible prevalence given case notifications ------------>
 
 prev_floor_labels <- c(
-  '95% UI bounds above\nprevalence floor\n',
-  'Lower bound of 95%\nUI below prevalence floor\n',
-  'Mean estimate below\nprevalence floor'
+  '95% UI bounds above prevalence floor',
+  'Lower bound of 95% UI below prevalence floor',
+  'Mean estimate below prevalence floor'
 )
 prev_floor_colors <- c(
   rgb(0, 0.6, 0.2, alpha = 0.5),
@@ -117,20 +126,21 @@ scatter_by_prev_floor <- ggplot(
   data = scatter_dt,
   aes(x = prev_floor, y = mean, ymin = lower, ymax = upper, color = pf_label)
 ) +
-  facet_wrap('plot_label') + 
+  facet_wrap('plot_label', ncol = 2) + 
   geom_abline(intercept=0, slope=1, linetype=2, lwd=.5, color='#888888') +
   geom_crossbar(show.legend = FALSE) +
   geom_point(size = 2) + 
   scale_color_manual(values = prev_floor_colors) +
-  scale_x_continuous(limits = c(0, 1500), oob = scales::squish) + 
-  scale_y_continuous(limits = c(0, 2000), oob = scales::squish) +
+  scale_x_continuous(limits = c(0, 1500), oob = scales::squish, labels = scales::comma) + 
+  scale_y_continuous(limits = c(0, 2000), oob = scales::squish, labels = scales::comma) +
   labs(
     title = "Out-of-sample comparison to 'prevalence floor' from case notifications",
-    color = "Estimate compared\nto prevalence floor",
+    color = "Estimate compared to prevalence floor",
     x = "Minimum reasonable prevalence given case notifications",
     y = 'Out-of-sample modeled TB prevalence'
   ) + 
-  theme_bw()
-png(file.path(viz_dir, 'prev_floor_scatter.png'), height=5, width=10, units='in', res=300)
+  theme_bw() + 
+  theme(legend.position = c(.75, .25))
+png(file.path(viz_dir, 'prev_floor_scatter.png'), height=7.5, width=7.5, units='in', res=300)
 print(scatter_by_prev_floor)
 dev.off()
